ee79d356ff5976b339090c02162248027b23dcfb
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-10-05 01:54:46 +0800

    feat: create logger middleware

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { userRouter } from './routes/user.route';
 import { env } from './config/env';
+import { loggerMiddleware } from './middleware/loggerMiddleware';
 
 const app = express();
 
+app.use(loggerMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
--- middleware/loggerMiddleware.ts
+++ middleware/loggerMiddleware.ts
@@ -1,39 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
-// format to human readable date
-const formatDate = (date: Date) => {
-  return date.toLocaleString('en-GB', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-  });
-};
-
 export const loggerMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
 ): void => {
-  const startTime = Date.now();
-  const { method, url } = req;
+  const method = req.method;
+  const route = req.url;
+  const timestamp = new Date().toISOString();
 
-  //log process start
-  console.log(`[${formatDate(new Date())}] [REQ] "${method} ${url}"`);
-
-  // log process finish time
-  res.on('finish', () => {
-    const duration = Date.now() - startTime;
-    const { statusCode } = res;
-    const logStatus =
-      statusCode >= 200 && statusCode < 300 ? '[GOOD]' : '[ERROR]';
-
-    console.log(
-      `[${formatDate(new Date())}] [RES] ${logStatus} "${method} ${url} ${statusCode}" [${duration}ms]`,
-    );
-  });
+  console.log(`[${timestamp}] ${method} ${route}`);
 
   next();
 };
