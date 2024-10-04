bc1ee5ad499195d0c3e500db7a36cb22a70aa70f
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-10-05 02:10:57 +0800

    nit: rename limiter to rateLimitMiddleware

--- app.ts
+++ app.ts
@@ -2,15 +2,18 @@ import express from 'express';
 import { userRouter } from './routes/user.route';
 import { env } from './config/env';
 import { loggerMiddleware } from './middleware/loggerMiddleware';
-import { limiter } from './middleware/rateLimitMiddleware';
+import { rateLimitMiddleware } from './middleware/rateLimitMiddleware';
 
 const app = express();
 
-app.use(limiter);
+// MIDDLEWARE
+app.use(rateLimitMiddleware);
 app.use(loggerMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(userRouter);
+
+// ROUTES
+app.use('/', userRouter);
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`);
--- middleware/rateLimitMiddleware.ts
+++ middleware/rateLimitMiddleware.ts
@@ -1,6 +1,6 @@
 import { rateLimit } from 'express-rate-limit';
 
-export const limiter = rateLimit({
+export const rateLimitMiddleware = rateLimit({
   windowMs: 30000,
   max: 5,
   message: 'Request Limit reached. Please try again in 30 seconds',
